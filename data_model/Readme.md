3c729345e2bd504f9435d8c065a6dea58ca12588
matthias <matthias.fuchs>
2021-09-19 13:33:03 +0200

    model

---
+++ data_model/Readme.md
@@ -0,0 +1,9 @@
+# data model
+
+## v_1 
+
+The version for cassandra used types
+
+## v_2
+
+Version for AWS keyspaces - removed types
--- data_model/create_mycloud.sh
+++ data_model/create_mycloud.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 HOSTNAME=mycloud
+VERSION=v_2
 
-cqlsh.py ${HOSTNAME} -f 01_lane_type.cql
-cqlsh.py ${HOSTNAME} -f 02_heat_data_table.cql
-cqlsh.py ${HOSTNAME} -f 03_heat_ids.cql
-cqlsh.py ${HOSTNAME} -f 04_base_data.cql
-cqlsh.py ${HOSTNAME} -f 05_base_data.cql
+cqlsh.py ${HOSTNAME} -f ${VERSION}/01_lane_type.cql
+cqlsh.py ${HOSTNAME} -f ${VERSION}/02_heat_data_table.cql
+cqlsh.py ${HOSTNAME} -f ${VERSION}/03_heat_ids.cql
+cqlsh.py ${HOSTNAME} -f ${VERSION}/04_base_data.cql
+cqlsh.py ${HOSTNAME} -f ${VERSION}/05_base_data.cql
+
+# checks
+echo ""
+echo "check DATA from ${HOSTNAME} with Version ${VERSION}"
+echo ""
+cqlsh.py ${HOSTNAME} -f ${VERSION}/99_check_data.cql
